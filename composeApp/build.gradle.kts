4e6b2223dee3fb51fec23de0f46429c678824424
Anthony Serrato <anthony17serrato@gmail.com>
2024-12-14 22:34:25 -0800

    Simple Ktor Server implementation

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -24,6 +24,12 @@ kotlin {
         desktopMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.jetbrains.swing)
+            implementation("io.ktor:ktor-server-status-pages:3.0.2")
+            implementation("io.ktor:ktor-server-core:3.0.2")  // Ktor core server
+            implementation("io.ktor:ktor-server-netty:3.0.2") // Netty engine for Ktor server
+            implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.2")  // JSON serialization
+            implementation("io.ktor:ktor-server-content-negotiation:3.0.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")  // Kotlinx Serialization library
         }
     }
 }
