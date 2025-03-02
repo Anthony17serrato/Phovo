2401c6486647d1734ff5d55cf7fd5d851d958610
Anthony Serrato <anthony17serrato@gmail.com>
2025-03-02 14:36:57 -0800

    Get named dispatcher

--- data/photos/src/desktopMain/kotlin/com/serratocreations/phovo/data/photos/di/PlatformModule.desktop.kt
+++ data/photos/src/desktopMain/kotlin/com/serratocreations/phovo/data/photos/di/PlatformModule.desktop.kt
@@ -1,14 +1,14 @@
 package com.serratocreations.phovo.data.photos.di
 
+import com.serratocreations.phovo.core.common.di.IO_DISPATCHER
 import com.serratocreations.phovo.data.photos.db.dao.DesktopPhovoItemDao
 import com.serratocreations.phovo.data.photos.db.dao.PhovoItemDao
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
-import kotlinx.coroutines.Dispatchers
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 actual fun platformModule() = module {
-    // TODO get() dispatcher
-    single<PhovoItemDao?> { DesktopPhovoItemDao(Dispatchers.IO) }
+    single<PhovoItemDao?> { DesktopPhovoItemDao(get(named(IO_DISPATCHER))) }
     single<HttpClient> { HttpClient(OkHttp) }
 }
