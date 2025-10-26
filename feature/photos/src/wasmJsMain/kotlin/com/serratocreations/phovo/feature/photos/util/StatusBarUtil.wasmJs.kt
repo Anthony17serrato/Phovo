b18ac5b3ff149978ff6121957d894fe5aa1216ef
Anthony Serrato <anthony17serrato@gmail.com>
2025-10-25 18:18:08 -0700

    Fix CI

---
+++ feature/photos/src/wasmJsMain/kotlin/com/serratocreations/phovo/feature/photos/util/StatusBarUtil.wasmJs.kt
@@ -0,0 +1,8 @@
+package com.serratocreations.phovo.feature.photos.util
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun SetStatusBarAppearance(lightIcons: Boolean) {
+    //NO-OP
+}
