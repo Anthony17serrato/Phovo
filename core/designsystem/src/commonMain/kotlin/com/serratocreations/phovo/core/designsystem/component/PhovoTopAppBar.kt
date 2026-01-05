929445433f67b024d45899602090ea4f1ce32db4
Anthony Serrato <anthony17serrato@gmail.com>
2026-01-04 16:13:37 -0800

    [Feature]Winter break feature drop (#64)
    * Add nav3 lib/update dependencies/rename composeApp module to phovoApp
    
    * Replace deprecated compose references with toml library references

--- core/designsystem/src/commonMain/kotlin/com/serratocreations/phovo/core/designsystem/component/PhovoTopAppBar.kt
+++ core/designsystem/src/commonMain/kotlin/com/serratocreations/phovo/core/designsystem/component/PhovoTopAppBar.kt
@@ -1,7 +1,6 @@
 package com.serratocreations.phovo.core.designsystem.component
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -14,7 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.testTag
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PhovoTopAppBar(
     navigationIcon: ImageVector?,
